092f386878ccfa14e301c1b754d62c303a46b6a2
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2023-10-17 18:02:33 +0300

    “commit”

--- simple-shell/main.c
+++ simple-shell/main.c
@@ -2,28 +2,31 @@
 
 int main(void)
 {
-    char *prompt = "($) ";
-    
-    
-    /* if echo run the command once*/
-    if (!isatty(STDIN_FILENO)){
-        run();
-        return 0;
-    }
-    signal(SIGINT, contol_C);
-    /* run the application loop*/
+   int  exited = 0;
+    char *command = NULL, **args = NULL;
+    size_t command_size = 0;
+    ssize_t command_length = 0;
+
     while (1)
     {
-        _print(prompt);
-       if(run())
-       {
-         break;
-       }
+    if (isatty(STDIN_FILENO))
+		_print("($) ");
         
+    command_length = getline(&command, &command_size, stdin);    
+    isExist(command_length, command);
+    
+    if (command_length > 0 && command[command_length] == '\n')
+    {
+        command[command_length] = '\0';
     }
-   
-    return 0;
+    args = tokenize_command(command, " \n");
+    exited = execute(args);
+    }
+    free_Arguments(args);
+    free(command);
+    return exited;
 }
+
 
 
 
