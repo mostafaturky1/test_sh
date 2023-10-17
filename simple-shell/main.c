d17e7c23dd6b5de42e363b71956ba68b8cf4b46c
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2023-10-17 18:16:57 +0300

    “commit”

--- simple-shell/main.c
+++ simple-shell/main.c
@@ -7,11 +7,12 @@ int main(void)
     size_t command_size = 0;
     ssize_t command_length = 0;
     signal(SIGINT,contol_C);
-    while (1)
+    while (command_length != EOF)
     {
-    if (isatty(STDIN_FILENO))
-		_print("($) ");
-        
+        if (isatty(STDIN_FILENO))
+		{
+            _print("($) ");
+        }
     command_length = getline(&command, &command_size, stdin);    
     isExist(command_length, command);
     
