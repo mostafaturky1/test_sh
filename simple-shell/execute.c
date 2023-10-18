c40f3e4a308985c3589b03cdec065383111c3164
mostafa mohamed <mostafa.mohamed.chp@gmail.com>
2023-10-19 00:45:53 +0300

    “commit”

--- simple-shell/execute.c
+++ simple-shell/execute.c
@@ -42,36 +42,6 @@ int execute(char *arguments[])
 }
 
 /**
- * run - Read and execute a command from the user.
- *
- * This function reads a command from the user via standard input.
- *
- * Return: exit status of the executed command or 0 if no command is entered.
- */
-
-// int run(void)
-// {
-
-// 	int  exited = 0;
-// 	char *command = NULL, **args;
-// 	size_t command_size = 0;
-// 	ssize_t command_length = 0;
-
-// 	command_length = getline(&command, &command_size, stdin);
-// 	isExist(command_length, command);
-// 	if (command_length > 0 && command[command_length] == '\n')
-// 	{
-// 		command[command_length] = '\0';
-// 	}
-//     args = NULL;
-// 	args = tokenize_command(command, " \n");
-// 	exited = execute(args);
-// 	free_Arguments(args);
-// 	free(command);
-// 	return (exited);
-// }
-
-/**
  * isExist - Check for a command's existence or exit condition.
  * @command_length: The length of the command input.
  * @command: The command input itself (unused parameter).
